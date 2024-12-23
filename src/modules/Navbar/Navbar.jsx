060416d33c5d82c2a38f7bd77ff4a819414c45cb
Relyful <13381270+Relyful@users.noreply.github.com>
2024-12-23 18:23:40 +0100

    Turn list items in header to links

--- src/modules/Navbar/Navbar.jsx
+++ src/modules/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import styles from './Navbar.module.css'
 import shoppingCart from '../../assets/ShoppingCart.svg'
+import { Link } from 'react-router-dom'
 
 export default function Navbar () {
   return (
     <nav className="navBar">
       <ul className={styles.list}>
-        <li>Home</li>
-        <li>Shop</li>
+        <li><Link to='./'>Home</Link></li>
+        <li><Link to='./Shoppingcart'>Shop</Link></li>
       </ul>
       <img src={shoppingCart} alt="shopping cart" className={styles.cartImg} />
     </nav>
