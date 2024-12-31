518c9516fd07267635aedcde14d062bdc3f06159
Relyful <13381270+Relyful@users.noreply.github.com>
2024-12-31 11:49:10 +0100

    Realize how to mock fetch

--- src/modules/Shop/Shop.test.jsx
+++ src/modules/Shop/Shop.test.jsx
@@ -1,10 +1,32 @@
-import { describe, it, expect } from 'vitest'
+import { vi, describe, it, expect } from 'vitest'
 import { render, screen, waitFor } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import { act } from 'react'
 import Shop from './Shop'
 
+const mockData = { products: [
+  { id: 1, title: 'Product 1', price: 19.99 }, 
+  { id: 2, title: 'Product 2', price: 19.99 },
+  { id: 3, title: 'Product 3', price: 19.99 },
+  { id: 4, title: 'Product 4', price: 19.99 },
+  { id: 5, title: 'Product 5', price: 19.99 },
+  { id: 6, title: 'Product 6', price: 19.99 },
+  { id: 7, title: 'Product 7', price: 19.99 },
+  { id: 8, title: 'Product 8', price: 19.99 },
+  { id: 9, title: 'Product 9', price: 19.99 },
+  { id: 10, title: 'Product 10', price: 19.99 }
+]};
+
 describe('Shop.jsx', () => {
+
+  vi.stubGlobal('fetch', vi.fn(() =>
+    Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve(mockData),
+    })
+  ));
+
+
   it('Should render 10 inputs 1 for each item', async () => {
     act(() => {
       render(
